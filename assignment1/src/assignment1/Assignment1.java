7757e613347826dbfce3b0fd702c49a18635e9ba
Mikaela <mikaelavo@gmail.com>
2025-07-20 23:43:00 -0700

    finish assignment 1

---
+++ assignment1/src/assignment1/Assignment1.java
@@ -0,0 +1,12 @@
+package assignment1;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("CSC142 - Intro to Java");
+		System.out.println("I learned about the stitch statement in Module 1");
+
+	}
+
+}
